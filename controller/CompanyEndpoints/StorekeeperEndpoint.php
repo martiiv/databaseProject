519c10494183304fb99d5a84d0d42e408ec4d8e8
tormodmm <tormodmm@stud.ntnu.no>
2021-04-09 09:40:54 +0200

    Created a working StorekeeperEndpoint for storekeeper to change status of order

--- controller/CompanyEndpoints/StorekeeperEndpoint.php
+++ controller/CompanyEndpoints/StorekeeperEndpoint.php
@@ -1,7 +1,33 @@
 <?php
-
+require_once 'db/OrderModel.php';
+require_once 'db/ShipmentModel.php';
 
 class StorekeeperEndpoint
 {
+    public function handleRequest($uri, $requestMethod, $queries, $payload): array
+    {
+        switch ($requestMethod) {
+            case RESTConstants::METHOD_PUT:
+                return $this->handleUpdate($uri, $queries, $payload);
+            default: //TODO Throw exception
+        }
+    }
 
+    private function handleUpdate($uri, $queries, $payload): array
+    {
+        if ($uri[0] == "order" && count($uri) == 6) {
+
+            $resource = array();
+            $resource['order_no'] = $uri[2];
+            $resource['status'] = $uri[4];
+            $shipment_no = (new OrderModel())->updateResource($resource);
+
+            $res['result'] = "Order: " . $uri[2] . " successfully updated";
+            $res['status'] =  RESTConstants::HTTP_OK;
+            return $res;
+        }
+        else {
+            //TODO Throw exception
+        }
+    }
 }
