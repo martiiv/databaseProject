a171ac1d0ec9cd44b45be12d6133b9674801e6b4
martinwh <martinwh@stud.ntnu.no>
2021-04-19 11:18:57 +0200

    Updated physical database - removed invalid data

--- doc/physical_database/assignment_project_with_data.sql
+++ doc/physical_database/assignment_project_with_data.sql
@@ -422,13 +422,13 @@ CREATE TABLE `ski_type` (
 --
 
 INSERT INTO `ski_type` (`model`, `ski_type`, `temperature`, `grip_system`, `size`, `weight_class`, `description`, `historical`, `photo_url`, `retail_price`, `production_date`) VALUES
-('Active', 'classic', 'cold', 'wax', 142, '20-30', 'Bra ski', 0, 'bildet', 1200, '2021-03-20'),
-('Active Pro', 'skate', 'warm', 'intelligrip', 147, '30-40', 'Rævva ski', 0, 'bildet', 1400, '2021-04-02'),
-('Endurance', 'double-pole', 'warm', 'wax', 152, '40-50', 'Ok ski', 0, 'bildet', 1500, '2021-12-03'),
-('Intrasonic', 'classic', 'cold', 'intelligrip', 157, '40-50', 'Litt bra ski', 0, 'bildet', 1500, '2021-10-05'),
-('Race Pro', 'skate', 'warm', 'wax', 162, '50-60', 'Ræser ski', 0, 'bildet', 2200, '2021-04-28'),
-('Race Speed', 'double-pole', 'warm', 'intelligrip', 167, '70-80', 'Beste skia', 0, 'bildet', 36000, '2017-01-01'),
-('Redline', 'skate', 'cold', 'wax', 172, '80-90', 'Verste skia', 0, 'bildet', 200, '2012-07-22');
+('Active', 'classic', 'cold', 'wax', 142, '20-30', 'Bra ski', 0, NULL, 1200, '2021-03-20'),
+('Active Pro', 'skate', 'warm', 'intelligrip', 147, '30-40', 'Rævva ski', 0, NULL, 1400, '2021-04-02'),
+('Endurance', 'double-pole', 'warm', 'wax', 152, '40-50', 'Ok ski', 0, NULL, 1500, '2021-12-03'),
+('Intrasonic', 'classic', 'cold', 'intelligrip', 157, '40-50', 'Litt bra ski', 0, NULL, 1500, '2021-10-05'),
+('Race Pro', 'skate', 'warm', 'wax', 162, '50-60', 'Ræser ski', 0, NULL, 2200, '2021-04-28'),
+('Race Speed', 'double-pole', 'warm', 'intelligrip', 167, '70-80', 'Beste skia', 0, NULL, 36000, '2017-01-01'),
+('Redline', 'skate', 'cold', 'wax', 172, '80-90', 'Verste skia', 0, NULL, 200, '2012-07-22');
 
 -- --------------------------------------------------------
 
