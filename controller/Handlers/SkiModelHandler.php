3a66cd4804ed56818718616e133e5e42fa912836
Martin Iversen <martiiv@ntnu.no>
2021-04-26 11:12:05 +0200

    Created update ski type test

---
+++ controller/Handlers/SkiModelHandler.php
@@ -0,0 +1,36 @@
+<?php
+
+
+class SkiModelHandler
+{
+    public function getResource(int $id): ?array
+    {
+        return (new SkiModel())->getResource($id);
+    }
+
+    public function getCollection(): ?array
+    {
+        return (new SkiModel())->getCollection();
+    }
+
+    public function createResource(array $arr): ?int
+    {
+        return (new SkiModel())->createResource($arr);
+    }
+
+    /**
+     * @throws APIException
+     */
+    public function updateResource(array $arr): ?int
+    {
+        if ($arr['ski_model'] == "") throw new APIException(RESTConstants::HTTP_BAD_REQUEST, $message = "No ski model provided");
+        if ($arr['historical'] == 0) throw new APIException(RESTConstants::HTTP_BAD_REQUEST, $message = "Please use the value 1 to set the historical value!");
+
+        return (new SkiModel())->updateResource($arr);
+    }
+
+    public function deleteResource(int $id): ?string
+    {
+        return (new SkiModel())->deleteResource($id);
+    }
+}
--- tests/unit/SkiTest.php
+++ tests/unit/SkiTest.php
@@ -16,8 +16,35 @@ class SkiTest extends \Codeception\Test\Unit
     }
 
     // tests
-    public function createRecource()
+    public function testCreateRecource()
     {
+        $array = array(
+            'model'=> 'UnitTestSki',
+            'ski_type'=>'classic',
+            'temperature'=>'warm',
+            'grip_system'=>'wax',
+            'size'=>240,
+            'weight_class'=>'120-130',
+            'description'=>'Ski created for a unit test',
+            'historical'=>0,
+            'photo_url'=>'u/tull/unit_test_ski',
+            'retail_price'=>16000);
 
+        $skiModelHandler = new SkiModelHandler();
+        $newSki = $skiModelHandler -> createResource($array);
+        $this -> assertNotEquals(0,$newSki);
     }
+
+    public function testUpdateResource(){
+        $arr = array(
+            'ski_model' => 'Active',
+            'historical' => 1
+        );
+
+        $skiModelHandler = new SkiModelHandler();
+        $updateSkiModelStatement = $skiModelHandler->updateResource($arr);
+        $this->assertNotEquals(null,$updateSkiModelStatement);
+    }
+
+
 }
