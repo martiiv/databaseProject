859f3ab747c45a5c864f2327304ce4b7338b7d3b
Martin Iversen <martiiv@ntnu.no>
2021-05-04 20:37:00 +0200

    Created testUploadPlan to test production plan creation

--- tests/api/CompanyEndpointCest.php
+++ tests/api/CompanyEndpointCest.php
@@ -14,7 +14,10 @@ require_once 'Authorisation.php';
  *                  testChangeStateReady        DONE Change to ready
  *                  testGetOrderAvailable       DONE Get order on state
  *      Production Planner:
- *                  testUploadPlan              TODO Upload production plan
+ *                  testUploadPlan              DONE Upload production plan
+ *
+ * Minor improvements: TODO Check for same date testUploadPlan
+ *                     TODO Check if correct state change for each endpoint
  * @date 04.05.2021
  * @version 0.6
  */
@@ -31,7 +34,8 @@ class CompanyEndpointCest
      * Lets a customer-rep get orders based on state
      * @param ApiTester $I
      */
-    public function testGetOrderNew(\ApiTester $I){
+    public function testGetOrderNew(\ApiTester $I)
+    {
         $I->haveHttpHeader('accept', 'application/json');
         $I->haveHttpHeader('content-type', 'application/json');
 
@@ -40,16 +44,16 @@ class CompanyEndpointCest
         $I->seeResponseIsJson();
         $I->seeResponseMatchesJsonType([
             'order_no' => 'string',
-            'created'=>'string',
+            'created' => 'string',
             'total_price' => 'string',
             'status' => 'string',
             'customer_id' => 'string',
         ]);
 
-        $I->dontSeeResponseContainsJson(array(['status' => 'open','shipment_no' => NULL]));
-        $I->dontSeeResponseContainsJson(array(['status' => 'available','shipment_no' => NULL]));
-        $I->dontSeeResponseContainsJson(array(['status' => 'ready','shipment_no' => NULL]));
-        $I->seeResponseContainsJson(array(['status' => 'new','shipment_no' => NULL]));
+        $I->dontSeeResponseContainsJson(array(['status' => 'open', 'shipment_no' => NULL]));
+        $I->dontSeeResponseContainsJson(array(['status' => 'available', 'shipment_no' => NULL]));
+        $I->dontSeeResponseContainsJson(array(['status' => 'ready', 'shipment_no' => NULL]));
+        $I->seeResponseContainsJson(array(['status' => 'new', 'shipment_no' => NULL]));
     }
 
     /**
@@ -59,7 +63,8 @@ class CompanyEndpointCest
      * Lets a customer-rep get orders based on state
      * @param ApiTester $I
      */
-    public function testGetOrderReady(\ApiTester $I){
+    public function testGetOrderReady(\ApiTester $I)
+    {
         $I->haveHttpHeader('accept', 'application/json');
         $I->haveHttpHeader('content-type', 'application/json');
 
@@ -68,16 +73,16 @@ class CompanyEndpointCest
         $I->seeResponseIsJson();
         $I->seeResponseMatchesJsonType([
             'order_no' => 'string',
-            'created'=>'string',
+            'created' => 'string',
             'total_price' => 'string',
             'status' => 'string',
             'customer_id' => 'string',
         ]);
 
-        $I->dontSeeResponseContainsJson(array(['status' => 'new','shipment_no' => NULL]));
-        $I->dontSeeResponseContainsJson(array(['status' => 'open','shipment_no' => NULL]));
-        $I->dontSeeResponseContainsJson(array(['status' => 'available','shipment_no' => NULL]));
-        $I->seeResponseContainsJson(array(['status' => 'ready','shipment_no' => NULL]));
+        $I->dontSeeResponseContainsJson(array(['status' => 'new', 'shipment_no' => NULL]));
+        $I->dontSeeResponseContainsJson(array(['status' => 'open', 'shipment_no' => NULL]));
+        $I->dontSeeResponseContainsJson(array(['status' => 'available', 'shipment_no' => NULL]));
+        $I->seeResponseContainsJson(array(['status' => 'ready', 'shipment_no' => NULL]));
     }
 
     /**
@@ -88,7 +93,8 @@ class CompanyEndpointCest
      * Lets a customer-rep change orders based on state
      * @param ApiTester $I
      */
-    public function testChangeStateOpen(\ApiTester $I){
+    public function testChangeStateOpen(\ApiTester $I)
+    {
         $I->haveHttpHeader('accept', 'application/json');
         $I->haveHttpHeader('content-type', 'application/json');
 
@@ -110,7 +116,8 @@ class CompanyEndpointCest
      * Lets a customer-rep change orders based on state
      * @param ApiTester $I
      */
-    public function testChangeStateAvailable(\ApiTester $I){
+    public function testChangeStateAvailable(\ApiTester $I)
+    {
         $I->haveHttpHeader('accept', 'application/json');
         $I->haveHttpHeader('content-type', 'application/json');
 
@@ -143,29 +150,29 @@ class CompanyEndpointCest
 
         Authorisation::setAuthorisationToken($I);
 
-        $I->sendPost('storekeeper/ski',[
-            'Active'=>2,
-            'Redline'=>3
+        $I->sendPost('storekeeper/ski', [
+            'Active' => 2,
+            'Redline' => 3
         ]);
 
         $I->seeResponseCodeIs(\Codeception\Util\HttpCode::CREATED);
         $I->seeResponseIsJson();
         $I->seeResponseMatchesJsonType([
-            'product_no'=>'string',
-            'production_date'=>'string',
-            'ski_type'=>'string']);
+            'product_no' => 'string',
+            'production_date' => 'string',
+            'ski_type' => 'string']);
 
         $I->seeResponseContainsJson(array(
-            ['production_date'=>date("Y/m/d"),'ski_type'=>'Active',
-                'production_date'=>date("Y/m/d"),'ski_type'=>'Active',
-                'production_date'=>date("Y/m/d"),'ski_type'=>'Redline',
-                'production_date'=>date("Y/m/d"),'ski_type'=>'Redline',
-                'production_date'=>date("Y/m/d"),'ski_type'=>'Redline']));
-        $I->seeInDatabase('product',  ['production_date'=>date("Y/m/d"),'ski_type'=>'Active'],
-            ['production_date'=>date("Y/m/d"),'ski_type'=>'Active'],
-            ['production_date'=>date("Y/m/d"),'ski_type'=>'Redline'],
-            ['production_date'=>date("Y/m/d"),'ski_type'=>'Redline'],
-            ['production_date'=>date("Y/m/d"),'ski_type'=>'Redline']);
+            ['production_date' => date("Y/m/d"), 'ski_type' => 'Active',
+                'production_date' => date("Y/m/d"), 'ski_type' => 'Active',
+                'production_date' => date("Y/m/d"), 'ski_type' => 'Redline',
+                'production_date' => date("Y/m/d"), 'ski_type' => 'Redline',
+                'production_date' => date("Y/m/d"), 'ski_type' => 'Redline']));
+        $I->seeInDatabase('product', ['production_date' => date("Y/m/d"), 'ski_type' => 'Active'],
+            ['production_date' => date("Y/m/d"), 'ski_type' => 'Active'],
+            ['production_date' => date("Y/m/d"), 'ski_type' => 'Redline'],
+            ['production_date' => date("Y/m/d"), 'ski_type' => 'Redline'],
+            ['production_date' => date("Y/m/d"), 'ski_type' => 'Redline']);
     }
 
     /**
@@ -176,7 +183,8 @@ class CompanyEndpointCest
      * Lets a storekeeper change orders based on state
      * @param ApiTester $I
      */
-    public function testChangeStateReady(\ApiTester $I){
+    public function testChangeStateReady(\ApiTester $I)
+    {
         $I->haveHttpHeader('accept', 'application/json');
         $I->haveHttpHeader('content-type', 'application/json');
 
@@ -197,7 +205,8 @@ class CompanyEndpointCest
      * Lets a customer-rep get orders based on state
      * @param ApiTester $I
      */
-    public function testGetOrderAvailable(\ApiTester $I){
+    public function testGetOrderAvailable(\ApiTester $I)
+    {
         $I->haveHttpHeader('accept', 'application/json');
         $I->haveHttpHeader('content-type', 'application/json');
 
@@ -206,16 +215,71 @@ class CompanyEndpointCest
         $I->seeResponseIsJson();
         $I->seeResponseMatchesJsonType([
             'order_no' => 'string',
-            'created'=>'string',
+            'created' => 'string',
             'total_price' => 'string',
             'status' => 'string',
             'customer_id' => 'string',
         ]);
 
-        $I->dontSeeResponseContainsJson(array(['status' => 'open','shipment_no' => NULL]));
-        $I->dontSeeResponseContainsJson(array(['status' => 'ready','shipment_no' => NULL]));
-        $I->dontSeeResponseContainsJson(array(['status' => 'new','shipment_no' => NULL]));
-        $I->seeResponseIsJson(array(['status' => 'skis available','shipment_no' => NULL]));
+        $I->dontSeeResponseContainsJson(array(['status' => 'open', 'shipment_no' => NULL]));
+        $I->dontSeeResponseContainsJson(array(['status' => 'ready', 'shipment_no' => NULL]));
+        $I->dontSeeResponseContainsJson(array(['status' => 'new', 'shipment_no' => NULL]));
+        $I->seeResponseIsJson(array(['status' => 'skis available', 'shipment_no' => NULL]));
     }
 
+    /**
+     * Function for testing production plan creation
+     * Tests the following endpoint:
+     *      http://localhost/dbproject-33/planner/plan
+     */
+    public function testUploadPlan(\ApiTester $I)
+    {
+        $I->haveHttpHeader('accept', 'application/json');
+        $I->haveHttpHeader('content-type', 'application/json');
+
+        Authorisation::setAuthorisationToken($I);
+
+        $I->sendPost('planner/plan', [
+            [
+                'start' => "2023-02-15",
+                'end' => "2023-03-15",
+                'planner nr' => 10002,
+            ],
+            [
+                'Active'=>2,
+                'Endurance'=>3,
+                'Race Pro'=>1
+            ]]);
+
+        $I->seeResponseCodeIs(\Codeception\Util\HttpCode::CREATED);
+        $I->seeResponseIsJson();
+        $I->seeResponseMatchesJsonType([
+                'start_date' => "string",
+                'end_date' => "string",
+                'no_of_skis_per_day'=>'integer',
+                'production_planner_number' => 'integer',
+
+            'plan'=>[
+                'Active'=>'integer',
+                'Endurance'=>'integer',
+                'Race Pro'=>'integer'
+            ]]);
+
+        $I->seeResponseContainsJson(
+            [
+                'start_date' => "2023-02-15",
+                'end_date' => "2023-03-15",
+                'no_of_skis_per_day'=>6,
+                'production_planner_number' => 10002,
+            'plan'=>array(
+                'Active'=>2,
+                'Endurance'=>3,
+                'Race Pro'=>1
+            )]);
+
+        $I->seeInDatabase('production_plan',['start_date'=>'2023-02-15','end_date'=>'2023-03-15','no_of_skis_per_day'=>6,'production_planner_number'=>10002]);
+        $I->seeInDatabase('production_list',['amount'=>2,'production_plan_start_date'=>'2023-02-15','production_plan_end_date'=>'2023-03-15','ski_type_model'=>'Active']);
+        $I->seeInDatabase('production_list',['amount'=>3,'production_plan_start_date'=>'2023-02-15','production_plan_end_date'=>'2023-03-15','ski_type_model'=>'Endurance']);
+        $I->seeInDatabase('production_list',['amount'=>1,'production_plan_start_date'=>'2023-02-15','production_plan_end_date'=>'2023-03-15','ski_type_model'=>'Race Pro']);
+    }
 }
