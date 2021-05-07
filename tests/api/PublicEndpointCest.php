5f5075c35be698ad4a826e7b5c058338cad7f9e1
Martin Iversen <martiiv@ntnu.no>
2021-05-07 10:24:22 +0200

    Created structure for the final api tests

---
+++ tests/api/PublicEndpointCest.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * Class PublicEndpointCest for testing the public endpoint
+ * Tests:
+ *      getSkiModels TODO Get info on ski_types with model filter
+ */
+class PublicEndpointCest
+{
+    public function _before(ApiTester $I)
+    {
+    }
+
+    public function getSkiModels(){
+
+    }
+}
---
+++ tests/api/TransporterEndpointCest.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * Class TransporterEndpointCest for testing the transporter endpoint
+ * Tests:
+ *       getOrders      TODO      Get orders ready for shipment
+ *       changeShipment TODO  Change the state of the shipment after pickup
+ */
+class TransporterEndpointCest
+{
+    public function _before(ApiTester $I)
+    {
+    }
+
+    public function getOrders(){
+
+    }
+
+    public function changeShipment(){
+
+    }
+}
