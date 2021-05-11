1f119f0b927740a84341b6a8998143bfc0a597e8
Martin Iversen <martiiv@ntnu.no>
2021-05-11 12:40:42 +0200

    Implemented getPlanSummary test in API test, updated cookie in Authorisation.php and updated dump file

--- tests/api/Authorisation.php
+++ tests/api/Authorisation.php
@@ -4,8 +4,7 @@ class Authorisation
 {
     public static function setAuthorisationToken(ApiTester $I)
     {
-        $cookie = new Symfony\Component\BrowserKit\Cookie('auth_token', 'EENV2yeVpGZCT8eCKxWh19fz9SZ4bA1Wh19GGkoQd4T8bHYtALHoQB6f82qqMxoh');
+        $cookie = new Symfony\Component\BrowserKit\Cookie('auth_token', 'c9caceea4162fdad403fbdf926ebc9ebf6b9f37688fbb051c15913cc3058c739');
         $I->getClient()->getCookieJar()->set($cookie);
     }
-
 }
